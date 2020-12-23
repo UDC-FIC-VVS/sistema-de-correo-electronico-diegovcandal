97b5c32a6aa44b2b709372caf867b952728f4964
Diego Varela Candal <diego.vcandal@udc.es>
2020-12-23 23:29:45 +0100

    Eliminados tests generalizados en tests por propiedades y añadidos test de valores frontera a ArchivadorSimpleTest.

--- src/test/java/gal/udc/fic/vvs/email/archivador/ArchivadorSimpleTest.java
+++ src/test/java/gal/udc/fic/vvs/email/archivador/ArchivadorSimpleTest.java
@@ -1,6 +1,5 @@
 package gal.udc.fic.vvs.email.archivador;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -19,133 +18,60 @@ public class ArchivadorSimpleTest {
 	private static final String CONTENIDO_MENSAJE = "Contenido de Mensaje Test";
 
 	/**
-	 * Test del metodo obtenerNombre, de la clase ArchivadorSimple.
+	 * Test unitario, del método almacenarCorreo, de la clase ArchivadorSimple.
 	 * 
-	 * Comprueba que el nombre obtenido es el esperado.
+	 * <p>
+	 * 
+	 * Añade un mail correctamente a un ArchivadorSimple vacio y con el espacio
+	 * justo para aceptarlo
+	 * 
+	 * <p>
+	 * 
+	 * <ul>
+	 * <li>Nivel de la prueba: Test Unitario.
+	 * <li>Categoria de la prueba: Test funcional dinamico de caja negra y positivo.
+	 * <li>Mecanismo de selección de datos:
+	 * <ul>
+	 * <li>Valores frontera, el tamaño del mensaje es el maximo para poder ser
+	 * almacenado
+	 * </ul>
+	 * </ul>
 	 */
 	@Test
-	public void obtenerNombreTest() {
+	public void almacenarCorreoEspacioExactoTest() {
 
-		ArchivadorSimple as = new ArchivadorSimple(NOMBRE_ARCHIVADOR, ESPACIO_ARCHIVADOR);
-		assertEquals(NOMBRE_ARCHIVADOR, as.obtenerNombre());
-	}
-
-	/**
-	 * Test del metodo almacenarCorreo, de la clase ArchivadorSimple.
-	 * 
-	 * Añade un mail correctamente a un ArchivadorSimple vacio
-	 */
-	@Test
-	public void almacenarCorreoTest() {
-
-		ArchivadorSimple as = new ArchivadorSimple(NOMBRE_ARCHIVADOR, ESPACIO_ARCHIVADOR);
+		ArchivadorSimple as = new ArchivadorSimple(NOMBRE_ARCHIVADOR, CONTENIDO_MENSAJE.length() + 1);
 		Correo c = new Mensaje(new Texto(NOMBRE_MENSAJE, CONTENIDO_MENSAJE));
 
 		assertTrue(as.almacenarCorreo(c));
 	}
 
 	/**
-	 * Test del metodo almacenarCorreo, de la clase ArchivadorSimple.
+	 * Test unitario, del método almacenarCorreo, de la clase ArchivadorSimple.
 	 * 
-	 * Añade un mail a un ArchivadorSimple sin el suficiente espacio
+	 * <p>
+	 * 
+	 * Añade un mail a un ArchivadorSimple sin el suficiente espacio.
+	 * 
+	 * <p>
+	 * 
+	 * <ul>
+	 * <li>Nivel de la prueba: Test Unitario.
+	 * <li>Categoria de la prueba: Test funcional dinamico de caja negra y positivo.
+	 * <li>Mecanismo de selección de datos:
+	 * <ul>
+	 * <li>Valores frontera, el tamaño del mensaje es el minimo para no poder
+	 * almacenarse
+	 * </ul>
+	 * </ul>
 	 */
 	@Test
 	public void almacenarCorreoSinEspacioTest() {
 
-		ArchivadorSimple as = new ArchivadorSimple(NOMBRE_ARCHIVADOR, ESPACIO_ARCHIVADOR_INSUFICIENTE);
+		ArchivadorSimple as = new ArchivadorSimple(NOMBRE_ARCHIVADOR, CONTENIDO_MENSAJE.length());
 		Correo c = new Mensaje(new Texto(NOMBRE_MENSAJE, CONTENIDO_MENSAJE));
 
 		assertFalse(as.almacenarCorreo(c));
-	}
-
-	/**
-	 * Test del metodo obtenerEspacioTotal, de la clase ArchivadorSimple.
-	 * 
-	 * Obtiene el espacio total del ArchivadorSimple
-	 */
-	@Test
-	public void obtenerEspacioTotalTest() {
-
-		ArchivadorSimple as = new ArchivadorSimple(NOMBRE_ARCHIVADOR, ESPACIO_ARCHIVADOR);
-
-		assertEquals(ESPACIO_ARCHIVADOR, as.obtenerEspacioTotal());
-	}
-
-	/**
-	 * Test del metodo obtenerEspacioTotal, de la clase ArchivadorSimple.
-	 * 
-	 * Obtiene el espacio total del ArchivadorSimple despues de añadir un correo
-	 */
-	@Test
-	public void obtenerEspacioTotalDespuesDeAlmacenarTest() {
-
-		ArchivadorSimple as = new ArchivadorSimple(NOMBRE_ARCHIVADOR, ESPACIO_ARCHIVADOR);
-		Correo c = new Mensaje(new Texto(NOMBRE_MENSAJE, CONTENIDO_MENSAJE));
-
-		as.almacenarCorreo(c);
-
-		assertEquals(ESPACIO_ARCHIVADOR, as.obtenerEspacioTotal());
-	}
-
-	/**
-	 * Test del metodo obtenerEspacioDisponible, de la clase ArchivadorSimple.
-	 * 
-	 * Obtiene el espacio disponible del ArchivadorSimple
-	 */
-	@Test
-	public void obtenerEspacioDisponibleTest() {
-
-		ArchivadorSimple as = new ArchivadorSimple(NOMBRE_ARCHIVADOR, ESPACIO_ARCHIVADOR);
-
-		assertEquals(ESPACIO_ARCHIVADOR, as.obtenerEspacioDisponible());
-	}
-
-	/**
-	 * Test del metodo obtenerEspacioDisponible, de la clase ArchivadorSimple.
-	 * 
-	 * Obtiene el espacio disponible del ArchivadorSimple despues de añadir un
-	 * correo correctamente
-	 */
-	@Test
-	public void obtenerEspacioDisponibleDespuesDeAlmacenarTest() {
-
-		ArchivadorSimple as = new ArchivadorSimple(NOMBRE_ARCHIVADOR, ESPACIO_ARCHIVADOR);
-		Correo c = new Mensaje(new Texto(NOMBRE_MENSAJE, CONTENIDO_MENSAJE));
-
-		as.almacenarCorreo(c);
-
-		assertEquals(ESPACIO_ARCHIVADOR - c.obtenerTamaño(), as.obtenerEspacioDisponible());
-	}
-
-	/**
-	 * Test del metodo obtenerEspacioDisponible, de la clase ArchivadorSimple.
-	 * 
-	 * Obtiene el espacio disponible del ArchivadorSimple despues de añadir un
-	 * correo sin tener el espacio necesario
-	 */
-	@Test
-	public void obtenerEspacioDisponibleSinEspacioTest() {
-
-		ArchivadorSimple as = new ArchivadorSimple(NOMBRE_ARCHIVADOR, ESPACIO_ARCHIVADOR_INSUFICIENTE);
-		Correo c = new Mensaje(new Texto(NOMBRE_MENSAJE, CONTENIDO_MENSAJE));
-
-		as.almacenarCorreo(c);
-
-		assertEquals(ESPACIO_ARCHIVADOR_INSUFICIENTE, as.obtenerEspacioDisponible());
-	}
-
-	/**
-	 * Test del metodo obtenerDelegado, de la clase ArchivadorSimple.
-	 * 
-	 * Obtiene el Archivador delegado asignado al ArchivadorSimple, que sera un null
-	 * para esta implementacion de Archivador
-	 */
-	@Test
-	public void obtenerDelegadoTest() {
-
-		ArchivadorSimple as = new ArchivadorSimple(NOMBRE_ARCHIVADOR, ESPACIO_ARCHIVADOR_INSUFICIENTE);
-
-		assertEquals(null, as.obtenerDelegado());
 	}
 
 }
