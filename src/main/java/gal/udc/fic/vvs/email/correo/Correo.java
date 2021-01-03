f212285bd5eab968a2e7da36020df69b52f4384c
Diego Varela Candal <diego.vcandal@udc.es>
2021-01-03 19:26:48 +0100

    Añadido plugin SpotBugs para pruebas dinámicas de caja blanca.

--- src/main/java/gal/udc/fic/vvs/email/correo/Correo.java
+++ src/main/java/gal/udc/fic/vvs/email/correo/Correo.java
@@ -4,25 +4,36 @@ import java.util.Collection;
 
 public interface Correo {
 
-    public void establecerLeido(boolean leido);
-    public int obtenerNoLeidos();
-    public int obtenerTamaño();
-    public Integer obtenerIcono();
-    public String obtenerPreVisualizacion();
-    public String obtenerVisualizacion();
-    public String obtenerRuta();
-    public Collection explorar() throws OperacionInvalida;
-    public Collection buscar(String busqueda);
+	public void establecerLeido(boolean leido);
 
-    public void añadir(Correo correo) throws OperacionInvalida;
-    public void eliminar(Correo correo) throws OperacionInvalida;
-    public Correo obtenerHijo(int n) throws OperacionInvalida;
-    public Correo obtenerPadre();
+	public int obtenerNoLeidos();
 
-    public static final Integer ICONO_CARPETA = new Integer(1);
-    public static final Integer ICONO_MENSAJE = new Integer(2);
-    public static final Integer ICONO_NUEVO_MENSAJE = new Integer(3);
-    public static final Integer ICONO_MENSAJE_CON_ADJUNTOS = new Integer(4);
-    public static final Integer ICONO_NUEVO_MENSAJE_CON_ADJUNTOS = new Integer(5);
+	public int obtenerTamaño();
+
+	public Integer obtenerIcono();
+
+	public String obtenerPreVisualizacion();
+
+	public String obtenerVisualizacion();
+
+	public String obtenerRuta();
+
+	public Collection explorar() throws OperacionInvalida;
+
+	public Collection buscar(String busqueda);
+
+	public void añadir(Correo correo) throws OperacionInvalida;
+
+	public void eliminar(Correo correo) throws OperacionInvalida;
+
+	public Correo obtenerHijo(int n) throws OperacionInvalida;
+
+	public Correo obtenerPadre();
+
+	public static final Integer ICONO_CARPETA = Integer.valueOf(1);
+	public static final Integer ICONO_MENSAJE = Integer.valueOf(2);
+	public static final Integer ICONO_NUEVO_MENSAJE = Integer.valueOf(3);
+	public static final Integer ICONO_MENSAJE_CON_ADJUNTOS = Integer.valueOf(4);
+	public static final Integer ICONO_NUEVO_MENSAJE_CON_ADJUNTOS = Integer.valueOf(5);
 
 }
